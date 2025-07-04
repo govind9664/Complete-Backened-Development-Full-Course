ca1b8658ddf8a36eeffcbe90dbb78f53a3931bee
govind9664 <gr7796811@email.com>
2025-07-04 08:22:32 +0530

    Initial commit

---
+++ Chapter 16 Introduction to MongoDB/app.js
@@ -0,0 +1,36 @@
+// Core Module
+const path = require("path");
+
+// External Module
+const express = require("express");
+
+// Local Module
+const storeRouter = require("./routes/storeRouter");
+const hostRouter = require("./routes/hostRouter");
+const errorControllers = require("./controllers/errors");
+const rootDir = require("./utils/pathUtils");
+
+const app = express();
+
+app.set("view engine", "ejs");
+app.set("views", "views");
+
+app.use((req, res, next) => {
+  console.log(req.url, req.method);
+  next();
+});
+
+app.use(express.urlencoded());
+
+app.use(storeRouter);
+
+app.use(hostRouter);
+
+app.use(express.static(path.join(rootDir, "public")));
+
+app.use(errorControllers.pageNotFound);
+
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`Server running on http://localhost:${PORT}`);
+});
